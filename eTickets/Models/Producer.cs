2557c19c1752303122adb85415de760a1c000782
lukeschmidt02 <77812790+lukeschmidt02@users.noreply.github.com>
2024-01-16 18:20:41 -0500

    Models and base data

---
+++ eTickets/Models/Producer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Producer
+    {
+        [Key]
+        public int Id { get; set; } // because in a database needs a unique id
+        public string ProfilePictureURL { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+
+        // Relationships
+        public List<Movie> Movies { get; set; }
+    }
+}
